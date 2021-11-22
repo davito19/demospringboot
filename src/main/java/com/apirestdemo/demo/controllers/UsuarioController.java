da0799d8d5aba4e3507341fa9888e2e63eec3e4a
Davito19 <omardbonett@gmail.com>
2021-11-22 13:26:04 -0500

    init respository

---
+++ src/main/java/com/apirestdemo/demo/controllers/UsuarioController.java
@@ -0,0 +1,51 @@
+package com.apirestdemo.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.apirestdemo.demo.models.UsuarioModel;
+import com.apirestdemo.demo.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuario(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtnenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id) {
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if(ok) return "Se eliminó el usuario con id" + id;
+        return "No pudo eliminar el usuario con id" + id;
+    }
+}
